b26c19af2f5ec70534c52634b98b36a0882a2eeb
Alisher <al_nuridin@kbtu.kz>
2026-04-23 23:32:22 +0500

    Add Practice7 - Pygame games: Mickey's clock, music player, moving ball

---
+++ w9/mickeys_clock/main.py
@@ -0,0 +1,46 @@
+import os
+import pygame
+from clock import MickeyClock
+
+pygame.init()
+
+WIDTH = 800
+HEIGHT = 800
+WHITE = (255, 255, 255)
+
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Mickey Clock")
+
+fps_clock = pygame.time.Clock()
+
+BASE_DIR = os.path.dirname(__file__)
+IMAGES_DIR = os.path.join(BASE_DIR, "images")
+
+background = pygame.image.load(os.path.join(IMAGES_DIR, "clock.jpeg")).convert()
+right_hand = pygame.image.load(os.path.join(IMAGES_DIR, "rightarm.jpeg")).convert()
+left_hand = pygame.image.load(os.path.join(IMAGES_DIR, "leftarm.jpeg")).convert()
+
+right_hand.set_colorkey((255,255,255))
+left_hand.set_colorkey((255,255,255)) 
+
+mickey_clock = MickeyClock(
+    screen=screen,
+    center=(WIDTH // 2, HEIGHT // 2),
+    background=background,
+    right_hand=right_hand,
+    left_hand=left_hand
+)
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill(WHITE)
+    mickey_clock.draw()
+
+    pygame.display.flip()
+    fps_clock.tick(60)
+
+pygame.quit()
